a586dccdcd0e3e428375730143c81c05b9de7a41
heyset <c.inaciomatheus@gmail.com>
2022-01-12 18:35:07 -0300

    req-4: feat: adiciona modulo de sessions

---
+++ back-end/src/modules/user/session/index.js
@@ -0,0 +1,9 @@
+const Controller = require('./session.controller');
+const Model = require('./session.model');
+const Router = require('./session.router');
+
+module.exports = {
+  Controller,
+  Model,
+  Router,
+};
