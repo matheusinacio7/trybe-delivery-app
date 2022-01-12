fc16893e3d6f6522030641ad3b7ed92d13339aa9
heyset <c.inaciomatheus@gmail.com>
2022-01-12 17:13:56 -0300

    req-3: feat: adiciona tamanho minimo de senha

--- front-end/src/modules/validation/schemas/login.schema.js
+++ front-end/src/modules/validation/schemas/login.schema.js
@@ -1,6 +1,8 @@
 import { object, string } from 'yup';
 
+const MINIMUM_PASSWORD_LENGTH = 6;
+
 export default object().shape({
   email: string().email().required(),
-  password: string().required(),
+  password: string().required().min(MINIMUM_PASSWORD_LENGTH),
 });
