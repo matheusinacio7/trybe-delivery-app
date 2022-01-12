9574d427ddbdad9c8811e89492e9f7568ec293a2
heyset <c.inaciomatheus@gmail.com>
2022-01-12 18:59:33 -0300

    req-4: feat: adiciona modulo de router

--- back-end/src/modules/server/server.js
+++ back-end/src/modules/server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const router = require('../router');
+
 const app = express();
+
+app.use(router);
 
 module.exports = app;
---
+++ back-end/src/modules/user/index.js
@@ -0,0 +1,5 @@
+const router = require('./user.router');
+
+module.exports = {
+  router,
+};
