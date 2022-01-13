8a9ae8b4bad6202c444503b45003e332d8f30e3b
heyset <c.inaciomatheus@gmail.com>
2022-01-13 17:56:53 -0300

    Adiciona request de login no front

--- front-end/src/modules/http/http.js
+++ front-end/src/modules/http/http.js
@@ -4,7 +4,15 @@ export const post = ({ url, body }) => fetch(url, {
     'Content-Type': 'application/json',
   },
   body: JSON.stringify(body),
-});
+})
+  .then((response) => response.json())
+  .then((result) => {
+    if (result.error) {
+      throw result.error;
+    }
+
+    return result;
+  });
 
 export const get = ({ url }) => fetch(url, {
   method: 'GET',
