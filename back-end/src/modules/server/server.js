a3142cbd79694733ea63c7df153d8dd5b56aaeda
heyset <c.inaciomatheus@gmail.com>
2022-01-13 17:37:27 -0300

    req-4: feat: adiciona helmet e cors

--- back-end/src/modules/server/server.js
+++ back-end/src/modules/server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const { handleError } = require('../middlewares');
 const router = require('../router');
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json(),
+  cors(),
+  helmet()
+);
 
 app.use(router);
 app.use(handleError);
