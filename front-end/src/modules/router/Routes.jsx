89a1f0f6e854b1c052ce6cfa3293ddde706aa025
heyset <c.inaciomatheus@gmail.com>
2022-01-14 15:19:48 -0300

    req-6: feat: adiciona rota de registro

--- front-end/src/modules/router/Routes.jsx
+++ front-end/src/modules/router/Routes.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Navigate, Routes as ReactRouterRoutes, Route } from 'react-router-dom';
 
-import Login from '../login';
+import { Login } from '../login';
+import { Register } from '../register';
 
 export default function Routes() {
   return (
     <ReactRouterRoutes>
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <div>Produtos!</div> } />
     </ReactRouterRoutes>
   );
