fb708972d6a03447d4c03c866ca81bbce8fbb22e
heyset <c.inaciomatheus@gmail.com>
2022-01-14 15:55:18 -0300

    req-7: adiciona elementos de registro

--- front-end/src/modules/register/Register.view.jsx
+++ front-end/src/modules/register/Register.view.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-// import { Button, Form, FormProvider, Field, Heading } from '../shared';
+import { Button, Form, FormProvider, Field, Heading } from '../shared';
 
 export default function Register() {
   return (
     <main>
-      <h1>Registro!</h1>
+      <Heading level={ 1 }>Cadastro</Heading>
+      <FormProvider
+        initialValues={ { name: '', email: '', password: '' } }
+      >
+        {({ isValid }) => (
+          <Form>
+            <Field name="name" type="text" data-testid="common_register__input-name" />
+            <Field name="email" type="email" data-testid="common_register__input-email" />
+            <Field
+              name="password"
+              type="password"
+              data-testid="common_register__input-password"
+            />
+            <Button
+              disabled={ !isValid }
+              type="submit"
+              testId="common_register__button-register"
+            >
+              Cadastrar
+            </Button>
+          </Form>
+        )}
+      </FormProvider>
     </main>
   );
 }
