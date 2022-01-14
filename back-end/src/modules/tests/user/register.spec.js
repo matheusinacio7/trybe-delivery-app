711c1a7198aae01f515cd9adaedac35f012664ee
heyset <c.inaciomatheus@gmail.com>
2022-01-14 16:18:53 -0300

    req-9: test: adiciona teste para dados invalidos

---
+++ back-end/src/modules/tests/user/register.spec.js
@@ -0,0 +1,68 @@
+const { describe, it } = require('@jest/globals');
+const request = require('supertest');
+
+const server = require('../../server');
+
+describe('POST /user', () => {
+  const url = '/user';
+
+  const userWithInvalidEmail = {
+    name: 'Jest McTest User',
+    email: 'invalid@email',
+    password: '123456',
+  };
+
+  const userWithInvalidPassword = {
+    name: 'Jest McTest User',
+    email: 'valid@email.com',
+    password: '12345',
+  };
+
+  const userWithInvalidName = {
+    name: 'Jest',
+    email: 'valid@email.com',
+    password: '123456',
+  };
+
+  // const validUser = {
+  //   email: 'adm@deliveryapp.com',
+  //   password: '--adm2@21!!--',
+  // };
+
+  describe('returns a validation error with invalid', () => {
+    it('name', () => request(server)
+      .post(url)
+      .send(userWithInvalidName)
+      .expect(400)
+      .then((response) => {
+        expect(response.body.error.message).toBe('Invalid name');
+        expect(response.body.error.code).toBe('VALIDATION_ERROR');
+      }));
+
+    it('email', () => request(server)
+      .post(url)
+      .send(userWithInvalidEmail)
+      .expect(400)
+      .then((response) => {
+        expect(response.body.error.message).toBe('Invalid email');
+        expect(response.body.error.code).toBe('VALIDATION_ERROR');
+      }));
+    
+    it('password', () => request(server)
+      .post(url)
+      .send(userWithInvalidPassword)
+      .expect(400)
+      .then((response) => {
+        expect(response.body.error.message).toBe('Invalid password');
+        expect(response.body.error.code).toBe('VALIDATION_ERROR');
+      }));
+  });
+
+  // it('returns a token when the user exists', () => request(server)
+  //   .post(url)
+  //   .send(validUser)
+  //   .expect(200)
+  //   .then((res) => {
+  //     expect(res.body.token).toBeDefined();
+  //   }));
+});
