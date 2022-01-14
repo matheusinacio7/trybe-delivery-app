8660fc075e6772ce518fefab19fb1fd1289b0508
heyset <c.inaciomatheus@gmail.com>
2022-01-14 15:29:42 -0300

    req-6: fix: corrige erros de lint

--- front-end/src/modules/register/Register.view.jsx
+++ front-end/src/modules/register/Register.view.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { Button, Form, FormProvider, Field, Heading } from '../shared';
+// import { Button, Form, FormProvider, Field, Heading } from '../shared';
 
 export default function Register() {
   return (
     <main>
       <h1>Registro!</h1>
     </main>
-  )
+  );
 }
