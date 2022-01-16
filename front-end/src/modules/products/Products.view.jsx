bb51d08e0a1fa4b1e028097a91d9dcb94fc04a36
heyset <c.inaciomatheus@gmail.com>
2022-01-16 15:52:54 -0300

    req-12: feat: adiciona modulo de produtos no front

---
+++ front-end/src/modules/products/Products.view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../layout';
+
+export default function Products() {
+  return (
+    <Layout context="customer">
+      <main>
+        <h1>Produtos!</h1>
+      </main>
+    </Layout>
+  );
+}
