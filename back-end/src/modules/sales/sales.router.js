739c3143ec5ae1813068b5eee289f0cd252aea75
heyset <c.inaciomatheus@gmail.com>
2022-01-18 16:31:30 -0300

    req-22: feat: adiciona rota de sales

--- back-end/src/modules/sales/sales.router.js
+++ back-end/src/modules/sales/sales.router.js
@@ -3,6 +3,16 @@ const router = require('express').Router();
 const Controller = require('./sales.controller');
 const { validateToken } = require('../middlewares');
 
+router.get('/', validateToken, async (req, res) => {
+  const sales = await Controller.get({
+    userId: res.locals.user.id,
+    userRole: res.locals.user.role,
+    searchByCustomer: req.query.customer,
+  });
+
+  res.status(200).json({ sales });
+});
+
 router.post('/', validateToken, async (req, res) => {
   const sale = await Controller.create({
     userId: res.locals.user.id,
