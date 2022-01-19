e324c91ea9b646c41248b8b8d521844bd8af3f97
heyset <c.inaciomatheus@gmail.com>
2022-01-19 15:06:27 -0300

    req-27: test: adiciona teste para getSales pessoa vendedora

--- back-end/src/modules/tests/sales/getSales.spec.js
+++ back-end/src/modules/tests/sales/getSales.spec.js
@@ -44,4 +44,35 @@ describe('GET /sales', () => {
         expect(response.body.sales[0].totalPrice).toBe('100.00');
       }));
   });
+
+  describe.only('for a SELLER', () => {
+    const sellerId = 2;
+    let tokenHeader;
+
+    beforeAll(function () {
+      const token = sign({ id: sellerId, role: 'seller' });
+      tokenHeader = ['Authorization', token];
+    });
+
+    it.only('without search params, throws a forbidden error', () => request(server)
+      .get(url)
+      .set(...tokenHeader)
+      .expect(403));
+
+    it('with search params for a different seller, throws forbidden error', () => request(server)
+      .get(url)
+      .query({ seller: 1 })
+      .set(...tokenHeader)
+      .expect(403));
+
+    it('with matching search params, returns a list of sales', () => request(server)
+      .get(url)
+      .query({ seller: sellerId })
+      .set(...tokenHeader)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.sales.length).toBe(1);
+        expect(response.body.sales[0].totalPrice).toBe('100.00');
+      }));
+  });
 });
