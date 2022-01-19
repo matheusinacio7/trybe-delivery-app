2c422d1dc47a5a4ab96ee6a37cba30055cee8014
heyset <c.inaciomatheus@gmail.com>
2022-01-19 15:12:13 -0300

    req-27: feat: adiciona busca pro vendedor no back

--- back-end/src/modules/tests/sales/getSales.spec.js
+++ back-end/src/modules/tests/sales/getSales.spec.js
@@ -54,7 +54,7 @@ describe('GET /sales', () => {
       tokenHeader = ['Authorization', token];
     });
 
-    it.only('without search params, throws a forbidden error', () => request(server)
+    it('without search params, throws a forbidden error', () => request(server)
       .get(url)
       .set(...tokenHeader)
       .expect(403));
@@ -71,7 +71,7 @@ describe('GET /sales', () => {
       .set(...tokenHeader)
       .expect(200)
       .then((response) => {
-        expect(response.body.sales.length).toBe(1);
+        expect(response.body.sales.length).toBe(2);
         expect(response.body.sales[0].totalPrice).toBe('100.00');
       }));
   });
